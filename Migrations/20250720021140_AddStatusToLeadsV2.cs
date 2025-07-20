1d957f569254a2df5c833815583e2d60d8516883
giafhibbia <zaenalmuttaqien3@gmail.com>
2025-07-20 11:13:15 +0700

    feat(migration): add status field to Leads table

---
+++ Migrations/20250720021140_AddStatusToLeadsV2.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyAuthDemo.Migrations
+{
+    public partial class AddStatusToLeadsV2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Leads",
+                type: "int",
+                nullable: false,
+                defaultValue: 0); // default ke Pending (enum = 0)
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Leads");
+        }
+    }
+}
